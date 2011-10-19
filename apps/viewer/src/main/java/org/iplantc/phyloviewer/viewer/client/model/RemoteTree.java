f130e4aca9cd8c51b028d26f7d7586272be6d63e
Kristopher Urie <kurie@fieldmuseum.org>
2011-10-19 18:56:53 -0500

    add persistent ITree implementation

---
+++ apps/viewer/src/main/java/org/iplantc/phyloviewer/viewer/client/model/RemoteTree.java
@@ -0,0 +1,75 @@
+package org.iplantc.phyloviewer.viewer.client.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.iplantc.phyloviewer.shared.model.INode;
+import org.iplantc.phyloviewer.shared.model.Tree;
+
+@Entity
+public class RemoteTree extends Tree
+{	
+	private String name;
+	private byte[] hash;
+	
+	public RemoteTree() {
+		
+	}
+	
+	public RemoteTree(String name) {
+		this.name = name;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Override
+	public int getId()
+	{
+		return super.getId();
+	}
+
+	@Override
+	public void setId(int id)
+	{
+		super.setId(id);
+	}
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST) //many trees (having different user-assigned names, for example) could point at the same root node
+	@Override
+	public RemoteNode getRootNode()
+	{
+		return (RemoteNode) super.getRootNode();
+	}
+
+	@Override
+	public void setRootNode(INode node)
+	{
+		super.setRootNode(node);
+	}
+
+	@Override
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public byte[] getHash()
+	{
+		return hash;
+	}
+
+	public void setHash(byte[] hash)
+	{
+		this.hash = hash;
+	}
+}
