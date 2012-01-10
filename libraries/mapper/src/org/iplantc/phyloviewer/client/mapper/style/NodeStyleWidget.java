84a3b2ec815ada9d6ca7a26fa11edb9b7287413b
Kristopher Urie <kurie@fieldmuseum.org>
2012-01-10 13:32:13 -0600

    get available shapes from enum instead of hard coding in the xml

--- libraries/mapper/src/org/iplantc/phyloviewer/client/mapper/style/NodeStyleWidget.java
+++ libraries/mapper/src/org/iplantc/phyloviewer/client/mapper/style/NodeStyleWidget.java
@@ -1,15 +1,21 @@
 package org.iplantc.phyloviewer.client.mapper.style;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.iplantc.phyloviewer.shared.render.style.INodeStyle;
 import org.iplantc.phyloviewer.shared.render.style.INodeStyle.Shape;
 import org.iplantc.phyloviewer.shared.render.style.NodeStyle;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.text.shared.AbstractRenderer;
+import com.google.gwt.text.shared.Renderer;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiFactory;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HasValue;
-import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.ValueListBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public class NodeStyleWidget extends Composite
@@ -19,11 +25,12 @@ public class NodeStyleWidget extends Composite
 
 	@UiField HasValue<String> colorField;
 	@UiField HasValue<Double> sizeField;
-	@UiField ListBox shapeField;
+	@UiField ValueListBox<Shape> shapeField;
 
 	public NodeStyleWidget()
 	{
 		initWidget(uiBinder.createAndBindUi(this));
+		shapeField.setAcceptableValues(getShapes());
 	}
 
 	public INodeStyle getStyle()
@@ -36,12 +43,7 @@ public class NodeStyleWidget extends Composite
 			size = Double.NaN;
 		}
 
-		Shape shape = null;
-		String shapeText = shapeField.getItemText(shapeField.getSelectedIndex());
-		if (shapeText != null && !shapeText.isEmpty())
-		{
-			shape = Shape.valueOf(shapeText);
-		}
+		Shape shape = shapeField.getValue();
 		
 		NodeStyle style = new NodeStyle(color, size, shape);
 		return style;
@@ -51,6 +53,31 @@ public class NodeStyleWidget extends Composite
 	{
 		colorField.setValue(null);
 		sizeField.setValue(null);
-		shapeField.setSelectedIndex(0);
+		shapeField.setValue(null);
+	}
+	
+	private List<Shape> getShapes()
+	{
+		Shape[] allShapes = INodeStyle.Shape.values();
+		return Arrays.asList(allShapes);
+	}
+	
+	@UiFactory ValueListBox<Shape> createShapeField()
+	{
+		Renderer<Shape> shapeRenderer = new AbstractRenderer<INodeStyle.Shape>()
+		{
+			@Override
+			public String render(Shape shape)
+			{
+				if (shape == null)
+				{
+					return "";
+				}
+				
+				return shape.toString();
+			}
+		};
+		
+		return new ValueListBox<Shape>(shapeRenderer);
 	}
 }
